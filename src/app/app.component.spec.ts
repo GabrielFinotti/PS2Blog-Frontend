36d43ee5fc8e591c28acd6ab3a17e1d61c41acbd
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-21 19:57:05 -0300

    Updates footer e variables
    Implementação do footer e das variáveis scss do projeto!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'PS2-Blog' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PS2-Blog');
   });
 
   it('should render title', () => {
