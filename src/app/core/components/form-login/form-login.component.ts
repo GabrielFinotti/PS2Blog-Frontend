284d9fe96855d6b0624e8263b5b849f0d47b165a
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-14 13:39:34 -0300

    Formulário de login
    Construção do fomulário de login!

--- src/app/core/components/form-login/form-login.component.ts
+++ src/app/core/components/form-login/form-login.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-login.component.html',
-  styleUrl: './form-login.component.scss'
+  styleUrl: './form-login.component.scss',
 })
 export class FormLoginComponent {
-
+  // Variáveis do componente
+  protected formLogin!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formLogin = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+        ],
+      ],
+      userSave: [''],
+    });
+  }
 }
--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Componentes
+import { FormLoginComponent } from '../../components/form-login/form-login.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormLoginComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
