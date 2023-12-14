e6a5bcb8b7ff0f4d59b2ce493636c500c7400265
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-14 16:53:18 -0300

    Form Login
    Estruturação e formatação do formulário de login completo!

--- src/app/core/components/form-login/form-login.component.ts
+++ src/app/core/components/form-login/form-login.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
@@ -15,9 +21,15 @@ import {
   styleUrl: './form-login.component.scss',
 })
 export class FormLoginComponent {
-  // Variáveis do componente
+  // Seletores de elementos.
+  @ViewChildren('input') private inputs!: QueryList<ElementRef>;
+  @ViewChildren('label') private labels!: QueryList<ElementRef>;
+
+  // Variáveis do componente.
   protected formLogin!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+
+  constructor(private formBuilder: FormBuilder, private render: Renderer2) {
+    // Criação do grupo do formulário de login.
     this.formLogin = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: [
@@ -30,5 +42,26 @@ export class FormLoginComponent {
       ],
       userSave: [''],
     });
+  }
+
+  // Função para modificar o valor do label quando o input estiver focado.
+  protected onFocus(index: number) {
+    this.render.setStyle(
+      this.labels.toArray()[index].nativeElement,
+      'top',
+      '-17px'
+    );
+  }
+
+  // Função para modificar o valor do label quando o input perder o foco.
+  protected onBlur(index: number) {
+    // Verificando se o input está vazio antes de retornar o label para a posição inicial.
+    if (this.inputs.toArray()[index].nativeElement.value.length == 0) {
+      this.render.setStyle(
+        this.labels.toArray()[index].nativeElement,
+        'top',
+        '25%'
+      );
+    }
   }
 }
