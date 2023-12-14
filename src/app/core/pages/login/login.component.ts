841050d5c8915c47702171aba60379c4433fe47a
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-14 19:12:20 -0300

    Fixed Bugs
    Correções de UX/UI efetuada no componente de formulário!

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 // Componentes
 import { FormLoginComponent } from '../../components/form-login/form-login.component';
+import { FormCadastroComponent } from '../../components/form-cadastro/form-cadastro.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormLoginComponent],
+  imports: [CommonModule, FormLoginComponent, FormCadastroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
