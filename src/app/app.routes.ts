fdaa422c15ab18ac95f27d980cbde6de6433a18c
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-14 13:14:26 -0300

    Páginas e componentes
    Criação da página de login e dos componentes de formulário!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+];
---
+++ src/app/core/components/form-login/form-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-login.component.html',
+  styleUrl: './form-login.component.scss'
+})
+export class FormLoginComponent {
+
+}
