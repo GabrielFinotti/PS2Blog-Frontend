c034659f950fa14575c3760959cd74c492fa13ae
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-15 16:36:38 -0300

    Navbar
    Animação da barra de navegação para mobile concluída!

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -6,24 +6,27 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
   // Variáveis do componente
-  protected activeNav!: boolean
-  protected showNav!: string;
-  protected closeNav!: string;
+  protected activeNav!: boolean;
 
   constructor() {
-    this.activeNav = false
+    this.activeNav = false;
   }
 
-  protected onShowNav() {
-    if(this.activeNav) {
-      this.activeNav = false
+  // Função de ativação da navegação.
+  protected onShowNav(event: HTMLElement) {
+    if (!this.activeNav) {
+      this.activeNav = true;
+      console.log(event);
+      event.style.top = '0';
+      event.style.width = '55%';
     } else {
-      this.activeNav = true
+      this.activeNav = false;
+      event.style.top = '-1';
+      event.style.width = '0';
     }
   }
-
 }
