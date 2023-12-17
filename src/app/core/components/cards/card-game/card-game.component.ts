7de23af8e9329493bd8f9818e9ba305a4c01f26b
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-17 02:48:00 -0300

    Updates
    Organização dos arquivos dos componentes.
    Criação dos cartões de postagens, e cartões de games.
    Estruturação e formatação do cartão de postagem completo!

---
+++ src/app/core/components/cards/card-game/card-game.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-game.component.html',
+  styleUrl: './card-game.component.scss'
+})
+export class CardGameComponent {
+
+}
---
+++ src/app/core/components/cards/card-post/card-post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-post.component.html',
+  styleUrl: './card-post.component.scss'
+})
+export class CardPostComponent {
+
+}
