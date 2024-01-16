0cc3e00c963fdbd7d5279674a4b0da998e393f97
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-16 14:35:02 -0300

    Form Conta
    Estruturação e formatação do formulário de conta concluída.

--- src/app/core/components/forms/form-conta/form-conta.component.ts
+++ src/app/core/components/forms/form-conta/form-conta.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-conta',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-conta.component.html',
-  styleUrl: './form-conta.component.scss'
+  styleUrl: './form-conta.component.scss',
 })
 export class FormContaComponent {
+  // Variáveis do componente
+  protected formProfile!: FormGroup;
+  protected formEditor!: boolean;
 
+  constructor(private formBuilder: FormBuilder) {
+    // Criando o formulário do perfil
+    this.formProfile = formBuilder.group({
+      user: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+    // Inicializando o editor como false
+    this.formEditor = false;
+  }
+  // Função para ativar o editor
+  protected activeEditor() {
+    if (this.formEditor) {
+      this.formEditor = false;
+    } else {
+      this.formEditor = true;
+    }
+  }
 }
