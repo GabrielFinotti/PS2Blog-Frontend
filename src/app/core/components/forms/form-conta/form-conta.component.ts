7d92b2ddf66b4c2ca85a1aa123911c1337b93ddf
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-16 02:49:26 -0300

    Pag Conta
    Criação da página de configuração de conta.
    Formatação e estruturação do card da foto de perfil pronto.
    Iniciando o formulário para edição dos dados do usuário!

---
+++ src/app/core/components/forms/form-conta/form-conta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-conta',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-conta.component.html',
+  styleUrl: './form-conta.component.scss'
+})
+export class FormContaComponent {
+
+}
--- src/app/core/pages/conta/conta.component.ts
+++ src/app/core/pages/conta/conta.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Componentes
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { CardPerfilComponent } from '../../components/cards/card-perfil/card-perfil.component';
+import { FormContaComponent } from '../../components/forms/form-conta/form-conta.component';
+
 @Component({
   selector: 'app-conta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NavBarComponent,
+    FooterComponent,
+    CardPerfilComponent,
+    FormContaComponent,
+  ],
   templateUrl: './conta.component.html',
-  styleUrl: './conta.component.scss'
+  styleUrl: './conta.component.scss',
 })
-export class ContaComponent {
-
-}
+export class ContaComponent {}
