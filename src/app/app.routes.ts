f0107b6b2795bc4e97633b4e1ff71b166bbac413
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 14:36:32 -0300

    Restart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,8 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
-
-// Componentes
->>>>>>> parent of 2970a27 (Restart)
 import { LoginComponent } from './core/pages/login/login.component';
-import { DownloadsComponent } from './core/pages/downloads/downloads.component';
-import { ContaComponent } from './core/pages/conta/conta.component';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
-// Guardas
-import { accessGuard } from './core/guards/access.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
-  {
-    path: 'downloads',
-    component: DownloadsComponent,
-    canActivate: [accessGuard],
-  },
-  { path: 'conta', component: ContaComponent, canActivate: [accessGuard] },
   { path: '**', component: NotFoundComponent },
 ];
