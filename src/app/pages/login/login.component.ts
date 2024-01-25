3c18f8af92806b5a3e359ff8ec669a78890da76e
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 14:48:22 -0300

    Restart finality

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FooterComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
