6988c190ca92ab48686636c6246b63b6372538ff
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 14:39:08 -0300

    clean

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './core/pages/login/login.component';
-import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent },
 ];
