2bd890ccd40d2a8f478bf76112f0a54cb293349e
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 01:01:51 -0300

    Update access-router.service.ts

--- src/app/core/services/access-router.service.ts
+++ src/app/core/services/access-router.service.ts
@@ -5,13 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class AccessRouterService {
   constructor() {}
-  // Função para pegar os dados da session
+
   public getData() {
-    // Recuperando os dados
     if (typeof window !== 'undefined') {
       const email = sessionStorage.getItem('email');
       const password = sessionStorage.getItem('password');
-      // Verificando se os dados existem, se sim, retorna verdadeiro, se não, retorna falso
+
       if (email != (null || undefined) && password != (null || undefined)) {
         return true;
       }
