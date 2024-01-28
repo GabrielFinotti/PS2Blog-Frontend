a155010c60bd9899b38acec18a1213eab8dfd7db
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-28 20:07:21 -0300

    Login Form
    Construção do formulário de login, juntamente com a pág de login concluída!

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  @ViewChildren('input') private input!: QueryList<ElementRef>;
+  @ViewChildren('label') private label!: QueryList<ElementRef>;
+  protected loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private render: Renderer2) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+      userSave: [''],
+    });
+  }
+
+  protected sendForm() {
+    if (this.loginForm.valid) {
+    } else {
+    }
+  }
+
+  protected onFocus(index: number) {
+    if (this.input.toArray()[index].nativeElement.value.length == 0) {
+      this.render.setStyle(
+        this.label.toArray()[index].nativeElement,
+        'top',
+        '-20px'
+      );
+    }
+  }
+
+  protected onBlur(index: number) {
+    if (this.input.toArray()[index].nativeElement.value.length == 0) {
+      this.render.setStyle(
+        this.label.toArray()[index].nativeElement,
+        'top',
+        '25%'
+      );
+    }
+  }
 }
