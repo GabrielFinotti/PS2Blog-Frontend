6d04128919c005f2f6f32eedf685f8679eeab0b1
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-28 13:42:48 -0300

    Footer
    Rodapé concluído

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { RegisterFormComponent } from '../../components/register-form/register-form.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
-import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, NavBarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    LoginFormComponent,
+    RegisterFormComponent,
+    FooterComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
