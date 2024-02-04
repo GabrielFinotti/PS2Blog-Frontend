90381937d0fd3d46e673827200fabf905a248f1e
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-02-04 20:34:23 -0300

    Results Game List
    Adicionado renderização dos resultados de de games e páginas totais recuperadas mediante a busca dos jogos.

--- src/app/components/cards/game-card/game-card.component.ts
+++ src/app/components/cards/game-card/game-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GameList } from '../../../interfaces/game-list';
 import { GameListService } from '../../../shared/services/gameList/game-list.service';
 import { ErrorMessage } from '../../../interfaces/error-message';
@@ -10,12 +10,14 @@ import { ErrorMessage } from '../../../interfaces/error-message';
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss',
 })
-export class GameCardComponent {
+export class GameCardComponent implements OnInit {
   protected gameList!: GameList;
 
-  constructor(private gameListService: GameListService) {
+  constructor(private gameListService: GameListService) {}
+
+  ngOnInit(): void {
     this.gameListService.getGameList().subscribe(
-      (res) => {
+      (res: GameList) => {
         this.gameList = res;
       },
       (err: ErrorMessage) => {
--- src/app/pages/download/download.component.ts
+++ src/app/pages/download/download.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { GameCardComponent } from '../../components/cards/game-card/game-card.component';
+import { GameListService } from '../../shared/services/gameList/game-list.service';
+import { ErrorMessage } from '../../interfaces/error-message';
+import { GameList } from '../../interfaces/game-list';
 
 @Component({
   selector: 'app-download',
@@ -10,4 +13,42 @@ import { GameCardComponent } from '../../components/cards/game-card/game-card.co
   templateUrl: './download.component.html',
   styleUrl: './download.component.scss',
 })
-export class DownloadComponent {}
+export class DownloadComponent implements OnInit {
+  protected totalDocs!: number;
+  protected totalPages!: number;
+  protected activeResults!: boolean;
+
+  constructor(private gameListService: GameListService) {
+    this.activeResults = false;
+  }
+
+  ngOnInit(): void {
+    this.gameListService.getGameList().subscribe(
+      (res: GameList) => {
+        this.totalDocs = res.gameList.totalDocs;
+        this.totalPages = res.gameList.totalPages;
+      },
+      (err: ErrorMessage) => {
+        console.log(err.error.message);
+      }
+    );
+  }
+
+  protected showResults(img: HTMLImageElement, data: HTMLDivElement) {
+    if (!this.activeResults) {
+      img.style.rotate = 'none';
+
+      data.style.height = '59px';
+      data.style.opacity = '1';
+
+      this.activeResults = true;
+    } else {
+      img.style.rotate = '-90deg';
+
+      data.style.height = '0';
+      data.style.opacity = '0';
+
+      this.activeResults = false;
+    }
+  }
+}
