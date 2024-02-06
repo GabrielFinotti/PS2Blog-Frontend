acc44ebd8c217bd504d5cf5d7f5bc080644bc9ba
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-02-06 15:47:08 -0300

    Button pagination
    Aplicando informação visual no botão de paginação para quando desativado, sua opacidade fique baixo

--- src/app/components/pagination/game-card-pagination/game-card-pagination.component.ts
+++ src/app/components/pagination/game-card-pagination/game-card-pagination.component.ts
@@ -7,29 +7,32 @@ import {
   Output,
   SimpleChanges,
 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GameListService } from '../../../shared/services/gameList/game-list.service';
 import { GameList } from '../../../interfaces/game-list';
 
 @Component({
   selector: 'app-game-card-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-card-pagination.component.html',
   styleUrl: './game-card-pagination.component.scss',
 })
 export class GameCardPaginationComponent implements OnInit, OnChanges {
   @Output() private sendQuery = new EventEmitter<string>();
   @Input() public gameName!: string;
-  public currentPage!: number;
   protected gameListData!: GameList;
+  public currentPage!: number;
   public query!: string;
+  public stylePrevButton!: Record<string, string>;
+  public styleNextButton!: Record<string, string>;
 
   constructor(private gameListService: GameListService) {
     this.currentPage = 1;
   }
 
   ngOnInit(): void {
-    this.getGameListData();
+    this.getPaginationData();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -37,17 +40,18 @@ export class GameCardPaginationComponent implements OnInit, OnChanges {
       changes['gameName'].currentValue !== changes['gameName'].previousValue
     ) {
       this.currentPage = 1;
-      this.getGameListData();
+      this.getPaginationData();
     }
   }
 
-  protected getGameListData() {
+  protected getPaginationData() {
     this.query = `?page=${this.currentPage}&name=${this.gameName}`;
 
     this.gameListService.getGameList(this.query).subscribe((res: GameList) => {
       this.gameListData = res;
     });
 
+    this.disableButton();
     this.sendQuery.emit(this.query);
   }
 
@@ -55,7 +59,7 @@ export class GameCardPaginationComponent implements OnInit, OnChanges {
     if (this.currentPage < this.gameListData.gameList.totalPages) {
       this.currentPage++;
       this.query = this.gameListData.gameList.nextPage;
-      this.getGameListData();
+      this.getPaginationData();
     }
 
     window.scrollTo({
@@ -68,7 +72,7 @@ export class GameCardPaginationComponent implements OnInit, OnChanges {
     if (this.currentPage > 1) {
       this.currentPage--;
       this.query = this.gameListData.gameList.prevPage;
-      this.getGameListData();
+      this.getPaginationData();
     }
 
     window.scrollTo({
@@ -98,11 +102,27 @@ export class GameCardPaginationComponent implements OnInit, OnChanges {
       this.gameListData = res;
     });
 
+    this.disableButton();
     this.sendQuery.emit(this.query);
 
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+  }
+
+  public disableButton() {
+    this.stylePrevButton = {
+      opacity: this.currentPage === 1 ? '0.1' : '1',
+    };
+
+    if (this.gameListData) {
+      this.styleNextButton = {
+        opacity:
+          this.currentPage === this.gameListData.gameList.totalPages
+            ? '0.1'
+            : '1',
+      };
+    }
   }
 }
