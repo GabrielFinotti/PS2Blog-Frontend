45f7737c968d785056be5db4407c5b2b3e46afe8
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-02-10 23:04:50 -0300

    Services Games
    Adicionando o envio do cabeçalho da requisição para uso da api da listagem dos jogos!

--- src/app/shared/services/gameList/game-list.service.ts
+++ src/app/shared/services/gameList/game-list.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { GameList } from '../../../interfaces/game-list';
@@ -8,14 +8,36 @@ import { GameList } from '../../../interfaces/game-list';
 })
 export class GameListService {
   private url!: string;
+  private id!: string | null;
 
   constructor(private http: HttpClient) {
     this.url = 'http://localhost:3000/games';
   }
 
+  private getUserId() {
+    let header: HttpHeaders | undefined;
+
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('id') !== null) {
+        this.id = localStorage.getItem('id');
+      }
+      if (sessionStorage.getItem('id') !== null) {
+        this.id = sessionStorage.getItem('id');
+      }
+    }
+
+    if (this.id !== null) {
+      header = new HttpHeaders({
+        'User-Id': this.id,
+      });
+    }
+
+    return header;
+  }
+
   public getGameList(query: string): Observable<GameList> {
     return this.http
-      .get<GameList>(`${this.url}${query}`)
+      .get<GameList>(`${this.url}${query}`, { headers: this.getUserId() })
       .pipe(map(this.filterData));
   }
 
