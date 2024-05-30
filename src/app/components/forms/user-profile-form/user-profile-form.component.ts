9f15c922d436e5b2b11c82a00beab021fab4c066
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-05-30 20:25:00 -0300

    User Form
    Estrutura inicial do formulário de usuário pronta

--- src/app/components/forms/user-profile-form/user-profile-form.component.ts
+++ src/app/components/forms/user-profile-form/user-profile-form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ShowPassword } from '../../../enums/show-password';
 
 @Component({
   selector: 'app-user-profile-form',
   standalone: true,
   imports: [],
   templateUrl: './user-profile-form.component.html',
-  styleUrl: './user-profile-form.component.scss'
+  styleUrl: './user-profile-form.component.scss',
 })
 export class UserProfileFormComponent {
+  @ViewChild('input') private input!: ElementRef<HTMLInputElement>;
 
+  protected ShowPassUrl!: string;
+
+  constructor() {
+    this.ShowPassUrl = ShowPassword.show;
+  }
+
+  protected showPass() {
+    if (this.ShowPassUrl === ShowPassword.show) {
+      this.ShowPassUrl = ShowPassword.hidden;
+      this.input.nativeElement.type = 'text';
+    } else {
+      this.ShowPassUrl = ShowPassword.show;
+      this.input.nativeElement.type = 'password';
+    }
+  }
 }
