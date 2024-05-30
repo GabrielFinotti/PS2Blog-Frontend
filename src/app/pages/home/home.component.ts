7039a27cda0467a4715ae1c85131a5ee8e4e998f
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-05-30 18:23:05 -0300

    UI/UX fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { GamesCardsComponent } from '../../components/cards/games-cards/games-cards.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, GamesCardsComponent],
+  imports: [NgClass, NavbarComponent, FooterComponent, GamesCardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  protected isShowSearch!: boolean;
+
+  constructor() {
+    this.isShowSearch = false;
+  }
+
+  protected showSearch() {
+    this.isShowSearch = !this.isShowSearch;
+  }
+}
