e887f1de96e6392f7559485c954dbc2eade5cc07
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-05-31 00:40:29 -0300

    Features
    Adição da funcionalizada para ativar a edição do formulário de perfil dinamicamente

--- src/app/components/forms/user-profile-form/user-profile-form.component.ts
+++ src/app/components/forms/user-profile-form/user-profile-form.component.ts
@@ -1,10 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ShowPassword } from '../../../enums/show-password';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './user-profile-form.component.html',
   styleUrl: './user-profile-form.component.scss',
 })
@@ -12,9 +19,18 @@ export class UserProfileFormComponent {
   @ViewChild('input') private input!: ElementRef<HTMLInputElement>;
 
   protected ShowPassUrl!: string;
+  protected isEditProfile!: boolean;
+  protected updateProfile!: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.updateProfile = this.formBuilder.group({
+      user: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+
     this.ShowPassUrl = ShowPassword.show;
+    this.isEditProfile = false;
   }
 
   protected showPass() {
@@ -25,5 +41,15 @@ export class UserProfileFormComponent {
       this.ShowPassUrl = ShowPassword.show;
       this.input.nativeElement.type = 'password';
     }
+  }
+
+  protected editProfile() {
+    this.isEditProfile = !this.isEditProfile;
+
+    this.updateProfile.reset('');
+  }
+
+  protected submitProfile() {
+    this.isEditProfile = false;
   }
 }
