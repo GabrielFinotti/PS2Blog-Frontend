86c54e0765f9a50820213a0cdde994881f4807e8
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-06-04 16:59:15 -0300

    UI Fixes

---
+++ src/app/components/cards/info-card/info-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.scss'
+})
+export class InfoCardComponent {
+
+}
