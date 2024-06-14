d9095fda6c52b8527b0decc69b8f299778633691
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-06-14 14:17:23 -0300

    Page Access
    Toggle Forms

---
+++ src/app/enums/string-form.ts
@@ -0,0 +1,6 @@
+export enum StringForm {
+  loginForm = "Don't have a save game?",
+  toggleLogin = 'Load Save!',
+  registerForm = 'Has a save game?',
+  toggleResgister = 'Create Save!',
+}
--- src/app/pages/access/access.component.ts
+++ src/app/pages/access/access.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/forms/login-form/login-form.component';
 import { RegisterFormComponent } from '../../components/forms/register-form/register-form.component';
+import { StringForm } from '../../enums/string-form';
 
 @Component({
   selector: 'app-access',
@@ -9,4 +10,28 @@ import { RegisterFormComponent } from '../../components/forms/register-form/regi
   templateUrl: './access.component.html',
   styleUrl: './access.component.scss',
 })
-export class AccessComponent {}
+export class AccessComponent {
+  public isToggle!: boolean;
+  public textForm!: string;
+  public textToggle!: string;
+
+  constructor() {
+    this.isToggle = false;
+    this.textForm = StringForm.loginForm;
+    this.textToggle = StringForm.toggleResgister;
+  }
+
+  public toggleForm() {
+    if (this.isToggle) {
+      this.isToggle = !this.isToggle;
+
+      this.textForm = StringForm.registerForm;
+      this.textToggle = StringForm.toggleLogin;
+    } else {
+      this.isToggle = !this.isToggle;
+      
+      this.textForm = StringForm.loginForm;
+      this.textToggle = StringForm.toggleResgister;
+    }
+  }
+}
