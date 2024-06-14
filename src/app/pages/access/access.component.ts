96e69ca4cd12086691f45f2eda3c386bfc89889e
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-06-14 13:40:47 -0300

    Register Form

--- src/app/pages/access/access.component.ts
+++ src/app/pages/access/access.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/forms/login-form/login-form.component';
+import { RegisterFormComponent } from '../../components/forms/register-form/register-form.component';
 
 @Component({
   selector: 'app-access',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RegisterFormComponent],
   templateUrl: './access.component.html',
   styleUrl: './access.component.scss',
 })
