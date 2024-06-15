1a367cfa6e6d501bf70125d0e6e977a92200c940
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-06-15 01:50:30 -0300

    Footer

--- src/app/pages/access/access.component.ts
+++ src/app/pages/access/access.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/forms/login-form/login-form.component';
 import { RegisterFormComponent } from '../../components/forms/register-form/register-form.component';
 import { StringForm } from '../../enums/string-form';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-access',
   standalone: true,
-  imports: [LoginFormComponent, RegisterFormComponent],
+  imports: [LoginFormComponent, RegisterFormComponent, FooterComponent],
   templateUrl: './access.component.html',
   styleUrl: './access.component.scss',
 })
