caf37bbf0b8e92eda4968c1449ae6797bc8eeab6
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-06-20 13:51:55 -0300

    Exclusion
    Removed the animation from games cards due to a bug, working on fixing it!

--- src/app/components/cards/game-card/game-card.component.ts
+++ src/app/components/cards/game-card/game-card.component.ts
@@ -1,12 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Input,
-  QueryList,
-  Renderer2,
-  ViewChildren,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-card',
@@ -15,30 +7,8 @@ import {
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss',
 })
-export class GameCardComponent implements AfterViewInit {
+export class GameCardComponent {
   @Input() public list!: Array<{ name: string; size: string; href: string }>;
-  @ViewChildren('card') private cards!: QueryList<ElementRef<HTMLDivElement>>;
 
-  constructor(private render: Renderer2) {}
-
-  ngAfterViewInit(): void {
-    this.cards.changes.subscribe((res) => {
-      this.setClass();
-    });
-  }
-
-  private setClass() {
-    const cardsArray = this.cards.toArray();
-
-    let count = 250;
-
-    cardsArray.forEach((card) => {
-      this.render.removeClass(card.nativeElement, 'show-card');
-      setTimeout(() => {
-        this.render.addClass(card.nativeElement, 'show-card');
-      }, count);
-
-      count += 250;
-    });
-  }
+  constructor() {}
 }
